baee5c8d79f36272ee4d7f541b36f3e5a796e37e
Michael Hoisie <hoisie@meraki.com>
2015-12-14 13:46:07 -0800

    First commit

---
+++ test/tests.js
@@ -0,0 +1,56 @@
+var chai = require("chai");
+var expect = chai.expect;
+var Factory = require("../lib/factory");
+
+describe("Factory", function() {
+  before(function() {
+    Factory.register("point", {x: 12, y: 13});
+    Factory.register("3dpoint", {z: 14}, "point");
+  });
+
+  describe("register", function() {
+    it("should registration with a custom object type", function () {
+      function Point() {};
+      Factory.register("custom", {x: 12, y: 13}, null, Point)
+      var a = Factory.create("custom");
+      expect(a instanceof Point).to.eql(true);
+    });
+  });
+
+  describe("create", function () {
+    it("should create a new factory", function () {
+      var p = Factory.create("point");
+      expect(p.x).to.eql(12);
+      expect(p.y).to.eql(13);
+    });
+
+    it("should create inherited factories", function () {
+      var p = Factory.create("3dpoint");
+      expect(p.x).to.eql(12);
+      expect(p.y).to.eql(13);
+      expect(p.z).to.eql(14);
+    });
+  });
+
+  describe("sequence", function () {
+    it("should support a numberic sequence", function () {
+      Factory.register("pointseq", {x: Factory.sequence(), y: Factory.sequence()});
+      var p1 = Factory.create("pointseq");
+      var p2 = Factory.create("pointseq");
+      expect(p1.x).to.eql(1);
+      expect(p1.y).to.eql(1);
+      expect(p2.x).to.eql(2);
+      expect(p2.y).to.eql(2);
+    });
+
+    it("should support a string sequence", function () {
+      Factory.register("pointseqstr", {x: Factory.sequence("{i}"), y: Factory.sequence("{i}")});
+      var p1 = Factory.create("pointseqstr");
+      var p2 = Factory.create("pointseqstr");
+      expect(p1.x).to.eql("1");
+      expect(p1.y).to.eql("1");
+      expect(p2.x).to.eql("2");
+      expect(p2.y).to.eql("2");
+    });
+  });
+});
